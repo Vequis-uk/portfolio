d1a56b248bae8083b67b2d69cbdf1aa5baca101e
David Bonser <david@codapt.co.uk>
2024-11-01 11:43:33 +0000

    header styling sorted, was a bodge before, this is much more reliable

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,10 +10,10 @@ import {CommonModule, NgOptimizedImage} from '@angular/common';
       <div class="header-main-logo-container">
         <img class="header-main-logo" ngSrc="../../../assets/images/david-logo.png" alt="David Bonser Logo" height="288"
              width="457">
-        <div class="sonic-image-container">
-          <img class="sonic-image" ngSrc="../../../assets/images/sonic-idle.gif" alt="Sonic Idle" height="288"
-               width="288">
-        </div>
+      </div>
+      <div class="sonic-image-container">
+        <img class="sonic-image" ngSrc="../../../assets/images/sonic-idle.gif" alt="Sonic Idle" height="288"
+             width="288">
       </div>
     </div>
   `,
